43ee76a6b050fed3126108129bdaae17283d7b04
Yash <Yash997370@gmail.com>
2026-05-08 07:05:57 +0530

    updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: "notes-sharing-app-f5d59",
-  storageBucket: "notes-sharing-app-f5d59.firebasestorage.app",
-  messagingSenderId: "329462346831",
-  appId: "1:329462346831:web:a421a8bedeea40f007263a"
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
