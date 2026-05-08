974ea78a339c95e5987859f4b2a514501a30c05b
Yash <Yash997370@gmail.com>
2026-05-08 06:31:12 +0530

    secured firebase api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHSFrJekT5GOyvEAvaECsDkMFs3TrkjNk",
-  authDomain: "notes-sharing-app-f5d59.firebaseapp.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: "notes-sharing-app-f5d59",
   storageBucket: "notes-sharing-app-f5d59.firebasestorage.app",
   messagingSenderId: "329462346831",
-  appId: "1:329462346831:web:a421a8bedeea40f007263a",
+  appId: "1:329462346831:web:a421a8bedeea40f007263a"
 };
 
 const app = initializeApp(firebaseConfig);
